1a250e802a7ec8a1f0b0314b6689dc01f98b4966
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-20 21:48:49 +0000

    make calculator functional by checking and comparing current state of global variables

--- js/calc.js
+++ js/calc.js
@@ -1,5 +1,7 @@
-let a, b, op;
-let display = "";
+let a,
+  b = undefined;
+let op,
+  display = "0";
 const BUTTONS = document.querySelectorAll(".button");
 const NUMPAD = [...BUTTONS].filter((button) => button.dataset.numpad);
 const ACTIONS = [...BUTTONS].filter((button) => button.dataset.action);
@@ -7,7 +9,7 @@ const ACTIONS = [...BUTTONS].filter((button) => button.dataset.action);
 // operate() takes in two numbers and an operator
 // then performs the relevant operation.
 const operate = (a, b, operator) => {
-  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+  if (isNaN(a) || isNaN(b)) {
     return console.error(
       `Numbers (${[a, b]}) entered are not valid. Must be of type 'number'`
     );
@@ -35,12 +37,15 @@ const toDisplay = (inputString, replace = false) => {
       if (display.indexOf(".") !== -1) {
         return;
       }
-      if (display.length === 0) {
-        display += "0";
+      if (display[0] === "0") {
+        display += inputString;
+        refreshDisplay();
+        return;
       }
     }
     display += inputString;
   }
+  display = (parseFloat(display) - 0).toString();
   refreshDisplay();
 };
 
@@ -54,8 +59,8 @@ const clearDisplay = () => {
 };
 
 const flipSign = () => {
-  if (display.length === 0) return;
-  if (display[0] === "-") {
+  if (display === "0") return;
+  if (display.indexOf("-") !== -1) {
     display = display.slice(1);
   } else {
     display = "-" + display;
@@ -64,28 +69,62 @@ const flipSign = () => {
 };
 
 const toPercent = () => {
-  if (parseFloat(display) === 0 || display === "") return;
+  if (display === "") {
+    display = document
+      .querySelector("#output")
+      .textContent.replace(/[^0-9.]/g, "");
+  }
+
+  if (parseFloat(display) === 0) return;
   display = (parseFloat(display) / 100).toString();
   refreshDisplay();
+};
+
+const actionClick = (action) => {
+  if (["+", "-", "*", "/"].indexOf(action) !== -1) {
+    if (typeof a === "undefined") {
+      a = parseFloat(display);
+      op = action;
+      clearDisplay();
+    } else {
+      b = parseFloat(display);
+      toDisplay(operate(a, b, op).toString(), true);
+      a = parseFloat(display);
+      display = "";
+      b = undefined;
+    }
+  } else if (action === "=") {
+    if (a && op !== "") {
+      if (display === "") {
+        return;
+      }
+      toDisplay(operate(a, parseFloat(display), op).toString(), true);
+      a = undefined;
+      b = undefined;
+      op = "";
+    }
+  }
+  console.log(a, b, op, display);
 };
 
 const btnPress = (button) => {
   const action = button.target.dataset.action;
   switch (action) {
     case "AC":
+      a = b = op = undefined;
       clearDisplay();
       break;
     case "+/-":
+      if (display === "") {
+        display = document.querySelector("#output").textContent;
+      }
       flipSign();
       break;
     case "%":
       toPercent();
       break;
     default:
-      console.error(
-        `Invalid button press detected, (does it contain dataset "data-action"?):`,
-        button.target
-      );
+      actionClick(action);
   }
 };
 
