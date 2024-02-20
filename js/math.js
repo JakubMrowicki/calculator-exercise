aa00a38d8f601ade4e85e18835748cd3471c65f8
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-20 16:35:41 +0000

    create basic math operator functions

---
+++ js/math.js
@@ -0,0 +1,22 @@
+const Add = (a, b) => {
+  return a + b;
+};
+
+const Subtract = (a, b) => {
+  return a - b;
+};
+
+const Multiply = (a, b) => {
+  return a * b;
+};
+
+const Divide = (a, b) => {
+  return a / b;
+};
+
+module.exports = {
+  Add,
+  Subtract,
+  Multiply,
+  Divide,
+};
