f9f38fc61558b64a8abe58d390bffeb20b56b2f0
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-20 16:51:36 +0000

    add function operate to evaluate actions

---
+++ js/calc.js
@@ -0,0 +1,23 @@
+let a, b, op;
+
+// operate() takes in two numbers and an operator
+// then performs the relevant operation.
+const operate = (a, b, operator) => {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return console.error(
+      `Numbers (${[a, b]}) entered are not valid. Must be of type 'number'`
+    );
+  }
+  switch (operator) {
+    case "+":
+      return Add(a, b);
+    case "-":
+      return Subtract(a, b);
+    case "*":
+      return Multiply(a, b);
+    case "/":
+      return Divide(a, b);
+    default:
+      return console.error(`Operator ("${operator}") not supported.`);
+  }
+};
